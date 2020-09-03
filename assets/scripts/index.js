fc266f83d9a651ed765da7b94eae92064b5dbf77
mhans003 <michaeledwardhanson@gmail.com>
2020-09-02 17:18:04 -0700

    Cleaned up last refactor and added more comments.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,7 +7,7 @@ var currentHour;
 getCurrentDate(); 
 
 //Create the time slots when the page loads. 
-createTimeSlots(); 
+printTimes(times); 
 
 //Load the current events from local storage. 
 fillSavedEvents(); 
@@ -20,49 +20,37 @@ getCurrentColors();
 
 function getCurrentDate() {
     //Get the current date using Moment.js and display in the heading.
-
     currentDate = moment().format("dddd, MMMM Do YYYY"); 
     $("#current-date").text(currentDate); 
-    
-}
-
-function createTimeSlots() {
-    //Create the times and the time output that will be printed to the screen.
-
-    printTimes(times); 
 }
 
 function printTimes(times) {
-
     //Print out divs for each time block. 
     times.forEach((time) => {
-        //Make this time row. 
+        //Make this time row and set its id dynamically to the current time's military time. 
         var timeRow = $("<div>");
         timeRow.attr("class", "row time-row"); 
-        timeRow.attr("id", `time-row-${time.time}`); 
+        timeRow.attr("id", `time-row-${time.militaryTime}`); 
 
-        //Create div for time.
+        //Create div for time and set its id dynamically to the current time's military time; Output time using the timeString property.
         var timeDiv = $("<div>"); 
         timeDiv.attr("class", "col-sm-2 time-div"); 
-        timeDiv.attr("id", `time-div-${time.time}`);
-        //timeDiv.text(time.timeString());
+        timeDiv.attr("id", `time-div-${time.militaryTime}`);
         timeDiv.text(time.timeString); 
 
-        //Create div for content.
+        //Create div for content. Create its id dynamically from the current time's military time. 
         var contentDiv = $("<div>"); 
         contentDiv.attr("class", "col-sm-8 content-div");
-        contentDiv.attr("id", `content-div-${time.time}`); 
+        contentDiv.attr("id", `content-div-${time.militaryTime}`); 
         contentDiv.attr("contenteditable", "true"); 
-        contentDiv.text("Test"); 
+        contentDiv.text("Type something to do and click the save icon!"); 
 
-        //Create save button icon. 
+        //Create save button icon, also setting its id. 
         var saveDiv = $("<div>"); 
         saveDiv.attr("class", "col-sm-2 save-div"); 
-        saveDiv.attr("id", `save-button-${time.time}`);
-        saveDiv.attr("data-target", `content-div-${time.time}`); 
+        saveDiv.attr("id", `save-button-${time.militaryTime}`);
+        saveDiv.attr("data-target", `content-div-${time.militaryTime}`); 
         saveDiv.html("<i class='far fa-save'></i>"); 
-        
-        
 
         //Append time div, content div, and save div to the row.
         timeRow.append(timeDiv); 
@@ -72,60 +60,61 @@ function printTimes(times) {
         //Append the row to the timeslots container. 
         $("#timeslots").append(timeRow); 
 
-        //Add event listeners to buttons.
+        //Add event listeners to buttons to save whatever the user types in.
         saveDiv.on("click", saveTimeRows); 
     }); 
 }
 
 function saveTimeRows(event) {
-    //Save the current content div for a given block so that it persists in the browser.
+    //When the user chooses, save the current content div for a given block so that it persists in the browser's local storage. 
     console.log($(event.target).attr("data-target")); 
 
-    //Create the object holding the event data. 
+    //Create the object holding the event data using the current date, passing through the data of the appropriate time slot. 
     var calendarEvent = {
         date: currentDate,
         time: $(`#${$(event.target).attr("data-target")}`).attr("id"),
         content: $(`#${$(event.target).attr("data-target")}`).text()
     }; 
 
-    //console.log(calendarEvent); 
-
+    //Create a key for storing this particular calendar event in local storage. Parse to JSON. 
     var eventKey = `${calendarEvent.date}_${calendarEvent.time}`
-    //console.log(eventKey); 
     localStorage.setItem(eventKey, JSON.stringify(calendarEvent)); 
     
 }
 
 function fillSavedEvents() {
-    //Get saved events from local storage.
+    //Get saved events from local storage. Create empty array to store. 
     var events = []; 
 
     //Access the parsed objects that hold the saved events for the given day.
     for(var timeSlot = 0; timeSlot < times.length; timeSlot++) {
-        var thisTime = times[timeSlot].time; 
+        //Identify the current time in the loop. 
+        var thisTime = times[timeSlot].militaryTime; 
+        //Convert stored JSON event object back to a JS object. 
         var thisEvent = JSON.parse(localStorage.getItem(`${currentDate}_content-div-${thisTime}`)); 
-        //console.log(thisEvent); 
-
+   
         //If this time has a saved event (not null), place it in the events array.
         if(thisEvent) {
             events.push(thisEvent); 
         }
-
-        //Go through each event, and save its text into the correct calendar time row.
-        events.forEach((event) => {
-            $(`#${event.time}`).text(event.content); 
-        });
     }
+
+    //Go through each event (calendar event object), and save its text into the correct calendar time row.
+    events.forEach((event) => {
+        $(`#${event.time}`).text(event.content); 
+    });
     
 }
 
 function getCurrentColors() {
+    //Fill in the events with the appropriate color depending on past, present, future. 
+    
     currentHour = Number(moment().format("HH")); 
     console.log(currentHour); 
 
     //Loop through each time slot and color with the correct time color
     for(var timeIndex = 0; timeIndex < times.length; timeIndex++) {
-        console.log($(`#content-div-${times[timeIndex].time}`).text()); 
+        console.log($(`#content-div-${times[timeIndex].militaryTime}`).text()); 
 
     }
 }
--- assets/scripts/times.js
+++ assets/scripts/times.js
@@ -1,3 +1,4 @@
+//Create blueprint for times. Give a military time for identification and use of every hour, and time for outputting in 12-hour format. 
 class time {
     constructor(timeInput) {
         this.militaryTime = timeInput; 
@@ -6,77 +7,15 @@ class time {
     }
 }
 
-/*
-var times = [
-    {
-        time: 8,
-        timeString: function() {
-            return `${this.time}AM`; 
-        } 
-    },
-    {
-        time: 9,
-        timeString: function() {
-            return `${this.time}AM`; 
-        } 
-    },
-    {
-        time: 10,
-        timeString: function() {
-            return `${this.time}AM`; 
-        } 
-    },
-    {
-        time: 11,
-        timeString: function() {
-            return `${this.time}AM`; 
-        } 
-    },
-    {
-        time: 12,
-        timeString: function() {
-            return `${this.time}PM`; 
-        } 
-    },
-    {
-        time: 1,
-        timeString: function() {
-            return `${this.time}PM`; 
-        } 
-    },
-    {
-        time: 2,
-        timeString: function() {
-            return `${this.time}PM`; 
-        } 
-    },
-    {
-        time: 3,
-        timeString: function() {
-            return `${this.time}PM`; 
-        } 
-    },
-    {
-        time: 4,
-        timeString: function() {
-            return `${this.time}PM`; 
-        } 
-    },
-    {
-        time: 5,
-        timeString: function() {
-            return `${this.time}PM`; 
-        } 
-    },
-];
-*/
-
+//Empty array to hold preferred times. 
 var times = []; 
 
-for(let i = 8; i < 18; i++) {
-    times.push(new time(i)); 
+//Fill the times array with the desired times (using military time as a base). 
+for(let thisTime = 8; thisTime < 18; thisTime++) {
+    times.push(new time(thisTime)); 
 }
 
+//Test console log
 for(let i = 0; i < times.length; i++) {
     console.log(times[i]); 
 }
